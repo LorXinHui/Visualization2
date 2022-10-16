8e6ce24e04ecd6f2f6304c14a01f4dbbfef9f00e
LorXinHui <114121595+LorXinHui@users.noreply.github.com>
2022-10-16 21:56:49 +0800

    Add files via upload

---
+++ map_agriculture.js
@@ -0,0 +1,5 @@
+var vg_7 = "map_agriculture.vg.json";
+
+vegaEmbed("#map_agriculture", vg_7).then(function(result) {
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
